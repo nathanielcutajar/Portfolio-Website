8943afd595f8b188ffab4ee18e967a1ddfd136e6
Nathaniel Cutajar <nathanieldbest@gmail.com>
2020-05-05 23:57:04 +0200

    Added rain background

--- js/custom-js.js
+++ js/custom-js.js
@@ -3,13 +3,13 @@ function changeBackground(skill) {
     var body = document.body.style;
     switch (skill) {
         case "software":
-            backgroundColor = "white";
+            makeItRain();
             break;
         case "photography":
-            backgroundColor = "yellow";
+            backgroundColor = "#FF0";
             break;
         case "music":
-            backgroundColor = "blue";
+            backgroundColor = "#0000FF";
             break;
         case "default":
             backgroundColor = "#555555";
@@ -19,3 +19,35 @@ function changeBackground(skill) {
     }
     body.background = backgroundColor;
 }
+
+var makeItRain = function () {
+
+    //clear out everything
+    //$('.rain').empty();
+
+    var increment = 0;
+    var drops = "";
+
+    while (increment < 100) {
+        //couple random numbers to use for various randomizations
+        //random number between 98 and 1
+        var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+        //random number between 5 and 2
+        var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+        //increment
+        increment += randoFiver;
+        //add in a new raindrop with various randomizations to certain CSS properties
+        drops += '<div class="drop" style="left: ' + increment
+            + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100)
+            + '%; animation-delay: 0.' + randoHundo 
+            + 's; animation-duration: 0.5' + randoHundo 
+            + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo 
+            + 's; animation-duration: 0.5' + randoHundo 
+            + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo 
+            + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+    }
+
+    $('.rain.front-row').append(drops);
+}
+
+makeItRain();
