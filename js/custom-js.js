607aadc3a427781a872cce289d1992f0272dd678
Nathaniel Cutajar <nathanieldbest@gmail.com>
2020-05-06 23:14:35 +0200

    Fixed rain animation 2
    Window now doesn't need scroll
    Added splashes
    Slowed down rain

--- js/custom-js.js
+++ js/custom-js.js
@@ -28,23 +28,23 @@ var makeItRain = function () {
     var increment = 0;
     var drops = "";
 
-    while (increment < 100) {
+    while (increment < 90) {
         //couple random numbers to use for various randomizations
         //random number between 98 and 1
-        var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+        var randoHundo = (Math.floor(Math.random() * (98) + 1));
         //random number between 5 and 2
-        var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+        var randoFiver = (Math.floor(Math.random() * (4) + 2));
         //increment
         increment += randoFiver;
         //add in a new raindrop with various randomizations to certain CSS properties
         drops += '<div class="drop" style="left: ' + increment
-            + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100)
+            + '%; bottom: ' + ((randoFiver * 2) + 100)
             + '%; animation-delay: 0.' + randoHundo 
-            + 's; animation-duration: 0.5' + randoHundo 
+            + 's; animation-duration: 1.5' + randoHundo 
             + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo 
-            + 's; animation-duration: 0.5' + randoHundo 
+            + 's; animation-duration: 1.5' + randoHundo 
             + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo 
-            + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+            + 's; animation-duration: 1.5' + randoHundo + 's;"></div></div>';
     }
 
     $('.rain.front-row').append(drops);
