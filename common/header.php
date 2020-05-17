77a591e6d404512829c587c59b1ccaeb828ef3ae
Nathaniel Cutajar <nathanieldbest@gmail.com>
2020-05-17 13:50:28 +0200

    Added nav bar, and some structure to photography projects

--- common/header.php
+++ common/header.php
@@ -6,32 +6,26 @@ echo "<meta charset=\"utf-8\">
 <meta name=\"author\" content=\"Nathaniel Cutajar\">
 <meta name=\"robots\" content=\"index, nofollow\">
 <link rel=\"canonical\" href=\"https://www.nathanielcutajar.com\">
-<base ref=\"https://www.nathanielcutajar.com/\" target=\"_blank\">
+<base ref=\"https://www.nathanielcutajar.com/\" target=\"_self\">
 <link rel=\"shortcut icon\" href=\"../favicon/favicon.ico\">
 
-<!-- Latest compiled and minified CSS -->
-<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\">
+<!-- Bootstrap CSS -->
+<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css\">
 
 <!--Font Awesome Icons-->
 <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css\">
 
-<!--My custom styles-->
-<link rel=\"stylesheet\" href=\"../css/custom-styles.css\">
-
 <!--Reset styles-->
 <link rel=\"stylesheet\" href=\"../css/reset.css\">
+
+<!--My custom styles-->
+<link rel=\"stylesheet\" href=\"../css/custom-styles.css\">
 
 <!-- jQuery library -->
 <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>
 
-<!-- Popper JS -->
-<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js\"></script>
-
-<!-- Latest compiled JavaScript -->
+<!-- Bootstrap JS -->
 <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\"></script>
-
-<!--Anime JS-->
-<script src=\"https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.0/anime.min.js\"></script>
 
 <!--My custom Javascript-->
 <script src=\"../js/custom-js.js\"></script>"
--- photography/index.php
+++ photography/index.php
@@ -2,15 +2,47 @@
 <html lang="en">
 
 <head>
-<?php include '../common/header.php'?>
+    <?php include '../common/header.php' ?>
     <!--Page-Specific Items-->
     <title>Photography</title>
 </head>
 
-<body id="photographyBody">
-    <div>
-        PHOTOGRAPHY PAGE UNDER DEVELOPMENT
+<body id="photoBody">
+    <div class="topnav">
+        <a href="../index.php">
+            <i class="fas fa-home"></i>
+        </a>
+        <a class="active" href="#">Projects</a>
+        <a href="#">Studio</a>
+        <a href="#">Contact</a>
     </div>
+    <div id="photoTitle">Title</div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <a href="#">
+                    <div class="img-wrap">
+                        <div class="projectTitle">Potatoes</div>
+                        <div class="overlay"></div>
+                        <img src="../images/pages/photography/cover_images/fire-cover.jpg" class="img-fluid">
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <div class="img-wrap">
+                        <div class="projectTitle">Potatoes</div>
+                        <div class="overlay"></div>
+                        <img src="../images/pages/photography/cover_images/fire-cover.jpg" class="img-fluid">
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
 </body>
 
 </html>
