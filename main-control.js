7726091ece3fa34bbed228ce3d4d92024ebd2888
Nathaniel Cutajar <nathanieldbest@gmail.com>
2020-05-31 12:04:56 +0200

    Main page directs are corrected

--- main-control.js
+++ main-control.js
@@ -1,17 +1,21 @@
-const http = require('http');
+//Imports
 const fs = require('fs');
 const chalk = require('chalk');
 const express = require('express');
 const path = require('path');
+const tjs = require('templatesjs')
+
 const app = express();
+
+//Resource paths
 app.use(express.static('images'));
 app.use(express.static('js'));
 app.use(express.static('css'));
+app.use(express.static('favicon'));
 
 console.clear();
 const args = process.argv.slice(2);
 var portArg = args[0];
-const hostname = 'localhost';
 const defaultPort = 3000;
 
 var port = defaultPort;
@@ -19,7 +23,16 @@ if (portArg != null) {
     port = TryParseInt(portArg, defaultPort);
 }
 
+fs.readFile("index.html", function(err, data){
+    if(err) throw err;
+    tjs.dir = "common/";
+    tjs.setSync(data);
+})
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+app.get('/photography', (req, res) => res.sendFile(path.join(__dirname + '/photography/index.html')))
+app.get('/software', (req, res) => res.sendFile(path.join(__dirname + '/software/index.html')))
+app.get('/music', (req, res) => res.sendFile(path.join(__dirname + '/music/index.html')))
 
 app.listen(port, () => console.log(`Portfolio website running on http://localhost:${port}`))
 
